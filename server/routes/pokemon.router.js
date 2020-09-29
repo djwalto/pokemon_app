1c82297299e13879f60cba8a94427ac83c612477
David Walton <davidwalton6772@gmail.com>
2020-09-29 11:20:10 -0500

    get route for all pokemon created

---
+++ server/routes/pokemon.router.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = express.Router();
+
+// GET ALL POKEMON
+router.get();
+
+// GET ALL TYPES
+
+// GET POKEMON WITH TYPES
+
+module.exports = router;
